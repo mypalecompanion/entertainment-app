c1cebf0ba5acfb3d05782bd10ef9ad34858e4aff
Jack Kapock <mypalecompanion@gmail.com>
2024-05-20 00:33:38 +0200

    Docker compose configuration

--- app-react/vite.config.ts
+++ app-react/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 8000,
+    host: true,
+    watch: {
+      usePolling: true,
+    },
+  },
+  // server: {
+  //   host: true,
+  //   port: 8000,
+  //   watch: {
+  //     usePolling: true,
+  //   },
+  // },
+});
